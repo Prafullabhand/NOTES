b1801346355b91ae1f1df6aa1be22e858574cdb3
Prafulla Bhand <your-email@example.com>
2026-01-07 08:24:33 +0530

    Render start script added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,13 +2,11 @@ import "./loadEnv.js";
 import mongoose from "mongoose";
 import app from "./app.js";
 
-console.log("ENV CHECK:", process.env.MONGO_URI, process.env.CLOUDINARY_API_KEY);
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Mongo Connected");
-    app.listen(process.env.PORT, () =>
-      console.log("Backend running on port", process.env.PORT)
+    app.listen(process.env.PORT || 5000, () =>
+      console.log("Backend running on port", process.env.PORT || 5000)
     );
   })
   .catch(err => console.log("MONGO ERROR:", err));
